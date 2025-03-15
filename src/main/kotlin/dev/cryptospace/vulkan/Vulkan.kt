dd5b6ee831e0e8c705af732802262a761c54b9d8
Nico Müller <mueller@cryptospace.dev>
2025-03-15 18:17:54 +0100

    validation layer logger

--- src/main/kotlin/dev/cryptospace/vulkan/Vulkan.kt
+++ src/main/kotlin/dev/cryptospace/vulkan/Vulkan.kt
@@ -1,83 +1,28 @@
 package dev.cryptospace.vulkan
 
+import dev.cryptospace.vulkan.core.VkInstanceFactory
+import dev.cryptospace.vulkan.core.VulkanValidationLayerLogger
 import dev.cryptospace.vulkan.core.VulkanValidationLayers
-import dev.cryptospace.vulkan.utils.getLogger
-import dev.cryptospace.vulkan.utils.pushStringList
-import org.lwjgl.PointerBuffer
-import org.lwjgl.glfw.GLFWVulkan.glfwGetRequiredInstanceExtensions
-import org.lwjgl.system.MemoryStack
-import org.lwjgl.vulkan.VK10.VK_MAKE_VERSION
-import org.lwjgl.vulkan.VK10.VK_STRUCTURE_TYPE_APPLICATION_INFO
-import org.lwjgl.vulkan.VK10.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO
-import org.lwjgl.vulkan.VK10.VK_SUCCESS
-import org.lwjgl.vulkan.VK10.vkCreateInstance
 import org.lwjgl.vulkan.VK10.vkDestroyInstance
-import org.lwjgl.vulkan.VkApplicationInfo
-import org.lwjgl.vulkan.VkInstance
-import org.lwjgl.vulkan.VkInstanceCreateInfo
 
 const val VK_KHRONOS_VALIDATION_LAYER_NAME = "VK_LAYER_KHRONOS_validation"
 
 class Vulkan(
     private val useValidationLayers: Boolean,
-    private val validationLayerNames: List<String>
+    validationLayerNames: List<String>
 ) : AutoCloseable {
 
-    val instance: VkInstance = createInstance()
-    val validationLayers: VulkanValidationLayers =
-        VulkanValidationLayers(if (useValidationLayers) validationLayerNames else emptyList())
-
-    private fun createInstance(): VkInstance = MemoryStack.stackPush().use { stack ->
-        val appInfo = stack.createApplicationInfo()
-        val createInfo = stack.createInstanceCreateInfo(appInfo)
-        val instance = stack.createVulkanInstance(createInfo)
-
-        return VkInstance(instance[0], createInfo)
-    }
-
-    private fun MemoryStack.createApplicationInfo(): VkApplicationInfo {
-        val appInfo = VkApplicationInfo.malloc(this).apply {
-            sType(VK_STRUCTURE_TYPE_APPLICATION_INFO)
-            pApplicationName(ASCII("Hello Vulkan Application"))
-            applicationVersion(VK_MAKE_VERSION(1, 0, 0))
-            pEngineName(ASCII("Vulkan"))
-            engineVersion(VK_MAKE_VERSION(1, 0, 0))
-            apiVersion(VK_MAKE_VERSION(1, 0, 0))
-        }
-        return appInfo
-    }
-
-    private fun MemoryStack.createInstanceCreateInfo(appInfo: VkApplicationInfo): VkInstanceCreateInfo {
-        val glfwExtensions = glfwGetRequiredInstanceExtensions()
-        checkNotNull(glfwExtensions) { "Unable to get GLFW Vulkan extensions" }
-
-        logger.info("Using validation layers: $validationLayerNames")
-        val activatedLayers = pushStringList(validationLayerNames)
-
-        val createInfo = VkInstanceCreateInfo.malloc(this).apply {
-            sType(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO)
-            pApplicationInfo(appInfo)
-            ppEnabledLayerNames(activatedLayers)
-            ppEnabledExtensionNames(glfwExtensions)
-        }
-
-        return createInfo
-    }
-
-    private fun MemoryStack.createVulkanInstance(createInfo: VkInstanceCreateInfo): PointerBuffer {
-        val instance = mallocPointer(1)
-        val result = vkCreateInstance(createInfo, null, instance)
-        check(result == VK_SUCCESS) { "Failed to create Vulkan instance: $result" }
-        return instance
-    }
+    val validationLayers = VulkanValidationLayers(if (useValidationLayers) validationLayerNames else emptyList())
+    val instanceFactory = VkInstanceFactory(useValidationLayers, validationLayers)
+    val instance = instanceFactory.createInstance()
+    val validationLayerLogger = VulkanValidationLayerLogger(instance).apply { if (useValidationLayers) set() }
 
     override fun close() {
-        vkDestroyInstance(instance, null)
-    }
+        if (useValidationLayers) {
+            validationLayerLogger.destroy()
+        }
 
-    companion object {
-        @JvmStatic
-        private val logger = getLogger<Vulkan>()
+        vkDestroyInstance(instance, null)
     }
 
 }
--- src/main/kotlin/dev/cryptospace/vulkan/utils/Stacks.kt
+++ src/main/kotlin/dev/cryptospace/vulkan/utils/Stacks.kt
@@ -3,11 +3,15 @@ package dev.cryptospace.vulkan.utils
 import org.lwjgl.PointerBuffer
 import org.lwjgl.system.MemoryStack
 
+fun MemoryStack.pushStrings(vararg strings: String): PointerBuffer {
+    return pushStringList(strings.toList())
+}
+
 fun MemoryStack.pushStringList(list: List<String>): PointerBuffer {
     val pointer = mallocPointer(list.size)
 
-    list.forEachIndexed { index, item ->
-        pointer.put(index, ASCII(item))
+    list.forEach { item ->
+        pointer.put(ASCII(item))
     }
 
     pointer.flip()
