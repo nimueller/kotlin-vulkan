077bdbc380af255a987750595c25fdbb904a2eee
Nico Müller <mueller@cryptospace.dev>
2025-08-03 17:26:07 +0200

    started depth buffering

--- anvil-examples/src/main/kotlin/dev/cryptospace/anvil/app/DepthBuffering.kt
+++ anvil-examples/src/main/kotlin/dev/cryptospace/anvil/app/DepthBuffering.kt
@@ -1,22 +1,20 @@
 package dev.cryptospace.anvil.app
 
-import dev.cryptospace.anvil.core.DeltaTime
 import dev.cryptospace.anvil.core.MainLoop
 import dev.cryptospace.anvil.core.input.Key
 import dev.cryptospace.anvil.core.math.Mat4
 import dev.cryptospace.anvil.core.math.TexturedVertex2
 import dev.cryptospace.anvil.core.math.Vec2
 import dev.cryptospace.anvil.core.math.Vec3
-import dev.cryptospace.anvil.core.rendering.Mesh
 import dev.cryptospace.anvil.vulkan.VulkanEngine
 
 private const val ROTATION_DEGREES_PER_SECOND: Float = 45f
 
 fun main() {
-    TexturedQuad()
+    DepthBuffering()
 }
 
-class TexturedQuad {
+class DepthBuffering {
 
     private val vertices = listOf(
         TexturedVertex2(
@@ -56,34 +54,25 @@ class TexturedQuad {
             val imageResourceStream = TexturedQuad::class.java.getResourceAsStream(imageName)
                 ?: error("Image resource $imageName not found")
             engine.imageManager.loadImage(imageResourceStream)
-//            engine.camera.lookAt(Vec3(2f, 2f, 2f), Vec3(0f, 0f, 0f), Vec3(0f, 0f, 1f))
-//            engine.camera.lookInDirection(Vec3(0f, 0f, -1f), Vec3(0f, 0f, 1f))
             engine.camera.lookAt(Vec3(0f, 0f, 0f), Vec3(0f, 0f, -3f), Vec3(0f, 1f, 0f))
             engine.camera.movementEnabled = true
             engine.camera.rotationEnabled = true
             engine.window.captureCursor()
 
-            val mesh = engine.renderingSystem.uploadMesh(TexturedVertex2::class, vertices, indices)
+            val firstMesh = engine.renderingSystem.uploadMesh(TexturedVertex2::class, vertices, indices)
+            val secondMesh = engine.renderingSystem.uploadMesh(TexturedVertex2::class, vertices, indices)
 
-            MainLoop(engine).loop { deltaTime, glfw, renderingContext ->
-                updateModelMatrix(deltaTime, mesh)
+            MainLoop(engine).loop { _, glfw, renderingContext ->
+                firstMesh.modelMatrix = Mat4.identity.translate(Vec3(0f, 0f, -3f))
+                renderingContext.drawMesh(firstMesh)
 
-                renderingContext.drawMesh(mesh)
+                secondMesh.modelMatrix = Mat4.identity.translate(Vec3(0f, 0f, -4f))
+                renderingContext.drawMesh(secondMesh)
 
                 if (glfw.isKeyPressed(Key.ESCAPE)) {
                     glfw.window.requestClose()
                 }
             }
         }
-    }
-
-    private var rotationInDegrees: Double = 0.0
-
-    private fun updateModelMatrix(deltaTime: DeltaTime, mesh: Mesh) {
-        val deltaRotation = deltaTime.seconds * ROTATION_DEGREES_PER_SECOND
-//        rotationInDegrees += deltaRotation
-//        rotationInDegrees %= 360.0
-        mesh.modelMatrix = Mat4.identity.translate(Vec3(0f, 0f, -3f))
-//            .rotate(Math.toRadians(rotationInDegrees).toFloat(), Vec3(0f, 0f, 1f))
     }
 }
--- anvil-vulkan/src/main/resources/shaders/shader.frag
+++ anvil-vulkan/src/main/resources/shaders/shader.frag
@@ -8,6 +8,5 @@ layout (location = 1) in vec2 fragTexCord;
 layout (location = 0) out vec4 outColor;
 
 void main() {
-    //    outColor = texture(texSampler, fragTexCord);
-    outColor = vec4(fragColor, 1.0);
+    outColor = texture(texSampler, fragTexCord);
 }
